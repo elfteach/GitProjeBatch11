feb4df94a20a83deb999647b149ea10d0a7a8a3b
elif <rumelifkrks@gmail.com>
2020-11-17 22:28:18 +0300

    ikinci gun 2. commit

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -1,12 +1,13 @@
 
 public class BirinciGun {
-
 	
 	public static void main(String[] args) {
 		
 		System.out.println("23:57 - Birinci Gun");
 		System.out.println("ikinci Commit");
-		System.out.println("UCUNCU Commit");
 		
-	}
+		System.out.println("10:26 ikinci gun");
+		
+	} 
+	 
 }
